0e3116b311c805369f7bba4c348f3e9d84b4a984
noname804 <baapmanus007@gmail.com>
2024-08-29 17:19:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7396653530:AAEj2RyXPira9B9-RN8x3Xh5je297MEcs0M")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Rohit69")
 
   #Enter your group id here Get it by creating a group that group add your bot and ross bot then public it and type /id to get your id
-GROUP_ID=("GROUPid")
+GROUP_ID=("708120566")
 
 
 
